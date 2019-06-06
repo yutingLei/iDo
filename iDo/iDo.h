ad9aa131dbb4bf8e443f5540307770be838a1d59
lyt <1049373170@qq.com>
2019-06-06 16:21:34 +0800

    Init project

---
+++ iDo/iDo.h
@@ -0,0 +1,19 @@
+//
+//  iDo.h
+//  iDo
+//
+//  Created by admin on 2019/6/6.
+//  Copyright © 2019 Conjur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iDo.
+FOUNDATION_EXPORT double iDoVersionNumber;
+
+//! Project version string for iDo.
+FOUNDATION_EXPORT const unsigned char iDoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iDo/PublicHeader.h>
+
+
